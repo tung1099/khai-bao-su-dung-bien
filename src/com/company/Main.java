9eaaf9dee051d650629fb90dc1a420d479b3f44f
GnuT 268 <dtroyal1102@gmail.com>
2022-02-08 20:18:52 +0700

    Khai bao va su dung bien

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int i = 5;
+        float f = 15.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+        System.out.println("i= " + i);
+        System.out.println("f= " + f);
+        System.out.println("d= " + d);
+        System.out.println("c= " +c);
+        System.out.println("s= " + s);
+    }
+}
